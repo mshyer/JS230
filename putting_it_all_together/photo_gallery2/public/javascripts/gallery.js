e79e538eda3e5647bb8c7695e8e15d0ec6fdc2ba
Michael S <mshyer@outlook.com>
2023-02-08 13:58:41 -0500

    final files

--- putting_it_all_together/photo_gallery2/public/javascripts/gallery.js
+++ putting_it_all_together/photo_gallery2/public/javascripts/gallery.js
@@ -1,11 +1,13 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
   let slides = document.getElementById('slides');
   const photosTemplate = document.getElementById('photos');
   const photoInfoTemplate = document.getElementById('photo_information');
   const photoCommentsTemplate = document.getElementById('photo_comments');
   const photoComment = document.getElementById('photo_comment');
-  const photoList = document.querySelector('div#comments > ul');
+  const commentsList = document.querySelector('div#comments > ul');
+  const nextButton = document.querySelector('a.next');
+  const previousButton = document.querySelector('a.prev');
+  let currentPhoto = 0;
 
   const templates = {};
   let photos;
@@ -25,15 +27,63 @@ document.addEventListener('DOMContentLoaded', () => {
     })[0];
 
     let header = document.querySelector('section > header');
+    header.replaceChildren();
     header.insertAdjacentHTML('beforeend', templates.photo_information(photo));
+    let likeButton = document.querySelector('div.actions .like');
+    let favoriteButton = document.querySelector('div.actions .favorite');
   };
+
+  document.querySelector('section > header').addEventListener('click', (event) => {
+    event.preventDefault();
+    let button = event.target;
+    let buttonType = button.getAttribute('data-property');
+    if (buttonType) {
+      let href = button.getAttribute('href');
+      let text = button.textContent;
+      let dataId = button.getAttribute('data-id');
+      fetch(href, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+        body: 'photo_id=' + dataId,
+        //mode: 'no-cors',
+        //'Content-Type': 'application/json',
+      }).then(response => response.json())
+        .then(json => {
+          button.textContent = text.replace(/\d+/, json.total);
+        });
+    }
+  });
+
+  function updateComments(json) {
+    commentsList.insertAdjacentHTML('beforeend', templates.photo_comment(json));
+  }
+
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    //Get the form data
+    //Send a post request in URLencoded form
+    //Get the response
+    //add a new comment by loading a new partial template with the response
+    let formDataUrl = new URLSearchParams(new FormData(event.target));
+    let url = event.target.action;
+    fetch(url, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+      body: formDataUrl,
+    }).then(response => response.json())
+      .then(json => {
+        updateComments(json);
+        event.target.reset();
+      });
+  });
 
   const renderComments = function(idx) {
     fetch(`/comments?photo_id=${idx}`)
       .then(response => response.json())
       .then(json => {
         comments = json;
-        photoList.insertAdjacentHTML('beforeend', templates.photo_comments({comments: comments}));
+        commentsList.replaceChildren();
+        commentsList.insertAdjacentHTML('beforeend', templates.photo_comments({comments: comments}));
       });
   };
 
@@ -42,13 +92,53 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(json => {
       photos = json;
       renderPhotos();
-      renderPhotoInformation(photos[0].id);
-      renderComments(photos[0].id);
+      renderPhotoInformation(photos[currentPhoto].id);
+      renderComments(photos[currentPhoto].id);
     });
 
   function renderPhotos() {
     slides.insertAdjacentHTML('beforeend', templates.photos({ photos: photos }));
   }
+
+  function cyclePhotos(reverse = false) {
+    console.log(currentPhoto);
+    let figs = [...slides.children];
+    figs[currentPhoto].className = 'hide';
+    //figs[currentPhoto].style.opacity = '0';
+    if (!reverse) {
+      currentPhoto += 1;
+      if (currentPhoto >= figs.length) {
+        currentPhoto = 0;
+      }
+    } else {
+      currentPhoto -= 1;
+      if (currentPhoto < 0) {
+        currentPhoto = figs.length - 1;
+      }
+    }
+    //figs[currentPhoto].style.opacity = '1';
+    figs[currentPhoto].className = 'show';
+    renderComments(photos[currentPhoto].id);
+    renderPhotoInformation(photos[currentPhoto].id);
+
+  }
+
+  nextButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    cyclePhotos();
+  });
+
+  previousButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    cyclePhotos(true);
+  });
+
+
+
+    //photos.unshift(...photos.splice(1));
+    //renderPhotos();
+    //renderPhotoInformation(photos[0].id);
+    //renderComments(photos[0].id);
 
 
   //  function parseHTML(text) {
