c2b5f1fda4c6bdd76527bbe941be6bc7569c5e9b
Michael S <mshyer@outlook.com>
2023-02-12 15:35:41 -0500

    solutions

--- exercises/forms_form_validation/javascripts/p2.js
+++ exercises/forms_form_validation/javascripts/p2.js
@@ -1,75 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#registration_form');
-  // const firstNameInput = document.getElementById('first_name');
-  // const lastNameInput = document.getElementById('last_name');
-  // const emailInput = document.getElementById('email');
-  // const passwordInput = document.getElementById('password');
-  // const phoneInput = document.getElementById('phone_number');
-  // const inputListenersMap = {
-  //   'first_name': firstNameListener,
-  //   'last_name': lastNameListener,
-  //   'email': emailListener,
-  //   'password': passwordListener,
-  //   'phone_number': phoneNumberListener,
-  // };
-
-  // function firstNameListener(event) {
-  //   event.preventDefault();
-  //   let errorMessage = firstNameInput.parentElement.querySelector('.error-message');
-  //   if (firstNameInput.validity.valueMissing) {
-  //     errorMessage.classList.add('active');
-  //     errorMessage.textContent = 'Please enter a first name';
-  //   } else {
-  //     errorMessage.classList.remove('active');
-
-  //   }
-  // }
-
-  // function lastNameListener(event) {
-  //   if (lastNameInput.validity.valueMissing) {
-  //     lastNameInput.setCustomValidity('Please enter a last name');
-  //   } else {
-  //     lastNameInput.setCustomValidity('');
-  //   }
-  // }
-
-  // function emailListener(event) {
-  //   if (emailInput.validity.patternMismatch) {
-  //     emailInput.setCustomValidity('Email number must be in format xx@xx');
-  //   } else {
-  //     emailInput.setCustomValidity('');
-  //   }
-  // }
-
-  // function passwordListener(event) {
-  //   if (passwordInput.validity.tooShort) {
-  //     passwordInput.setCustomValidity('Password must be at least 10 chars long');
-  //   } else {
-  //     passwordInput.setCustomValidity('');
-  //   }
-  // }
-
-  // function phoneNumberListener(event) {
-  //   if (phoneInput.validity.patternMismatch) {
-  //     phoneInput.setCustomValidity('Phone number must be in format xxx-xxx-xxxx');
-  //   } else {
-  //     phoneInput.setCustomValidity('');
-  //   }
-  // }
-
-  // const inputs = document.querySelectorAll('input');
-  // for (let idx = 0; idx < inputs.length; idx += 1) {
-  //   inputs[idx].addEventListener('change', inputListenersMap[inputs[idx].id]);
-  // }
-
-  // form.addEventListener('submit', (event) => {
-  //   event.preventDefault();
-  //   //let validity = form.reportValidity();
-  //   //if (validity) {
-  //   //  form.submit();
-  //   //}
-
-  // });
   const App = {
     form: form,
     bindFocusEvents() {
@@ -82,6 +12,28 @@ document.addEventListener('DOMContentLoaded', () => {
       this.inputs.forEach(input => {
         input.addEventListener('blur', this.handleBlur.bind(this));
       });
+    },
+
+    bindKeypressEvents() {
+      document.getElementById('first_name').addEventListener('keypress', this.blockNonAlpha.bind(this));
+      document.getElementById('last_name').addEventListener('keypress', this.blockNonAlpha.bind(this));
+
+      document.getElementById('cd1').addEventListener('keypress', this.blockNonNumeric.bind(this));
+      document.getElementById('cd2').addEventListener('keypress', this.blockNonNumeric.bind(this));
+      document.getElementById('cd3').addEventListener('keypress', this.blockNonNumeric.bind(this));
+      document.getElementById('cd4').addEventListener('keypress', this.blockNonNumeric.bind(this));
+    },
+
+    blockNonAlpha(e) {
+      if (!e.key.match(/[a-zA-Z]/)) {
+        e.preventDefault();
+      }
+    },
+
+    blockNonNumeric(e) {
+      if (!e.key.match(/\d/)) {
+        e.preventDefault();
+      }
     },
 
     handleFocus(e) {
@@ -152,6 +104,7 @@ document.addEventListener('DOMContentLoaded', () => {
       this.inputs = document.querySelectorAll('input');
       this.bindFocusEvents();
       this.bindBlurEvents();
+      this.bindKeypressEvents();
       this.form.addEventListener('submit', this.handleFormSubmit.bind(this));
     },
   };
